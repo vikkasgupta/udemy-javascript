c1daadae38714de373aea821020997ffe6a2ede9
Vikas GUPTA <vikas.gupta@amadeus.com>
2022-12-27 19:01:07 +0530

    files

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,53 @@
+let js = "amazing";
+
+// console.log(40 + 2 + 98);
+let firstName = "Vikkas";
+
+console.log(firstName);
+console.log(firstName);
+
+// variable naming conventions
+let years = 3;
+let PI = 3.14159265;
+console.log(PI + 10);
+
+let $function = 'add';
+let $function2_vikkas_gupta = 'sample variable name';
+
+console.log($function);
+console.log($function2_vikkas_gupta);
+
+// this is a comment
+
+let country = 'India';
+let continent = 'Asia';
+let population = 1.5;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+let abc;
+console.log(abc);
+true;
+console.log(true);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+
+
+javascriptIsFun = 'no';
+console.log(typeof javascriptIsFun);
+console.log(typeof vikkas);
+
+let person = null;
+console.log(person);
+console.log(typeof null);
+
+/*
+	this is a multi line comment
+	note that the author of this file is Vikkas GUPTA
+	and the email address is contact@vikkasgupta.in
+
+*/
